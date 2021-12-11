5b35d81170be54f8655b3cd0ea6047f46e26d07b
raunaq <raunaq.soni@gmail.com>
2021-12-11 13:35:45 +0000

    chore: added django-env

--- wattx/wattx/settings.py
+++ wattx/wattx/settings.py
@@ -9,23 +9,32 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ
+import os
 from pathlib import Path
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1$%5^vqu%6foe=#mr^gjgveb^o4gj#iabx06fd(8#kk)#5(lhq'
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-1$%5^vqu%6foe=#mr^gjgveb^o4gj#iabx06fd(8#kk)#5(lhq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
